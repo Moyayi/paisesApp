c8ffa46bd628f28a7d553bf6358bb376f8c8049b
Pedro Moyano <pedro.moyano.garcia@gmail.com>
2023-02-02 21:39:32 +0100

    Display error when doesn't find any country with that searched value

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {HttpParams, HttpClient} from '@angular/common/http'
 import { PaisService } from '../../services/pais.service';
+import { Observer } from 'rxjs';
 
 @Component({
   selector: 'app-por-pais',
@@ -8,11 +9,24 @@ import { PaisService } from '../../services/pais.service';
   styleUrls: ['./por-pais.component.sass']
 })
 export class PorPaisComponent {
-
+  private _Error : boolean = false;
   busqueda : string = "";
   constructor(private servicio : PaisService){}
 
   buscar(){
-    this.servicio.buscarPais(this.busqueda).subscribe((resp) => console.log(resp))
+    this._Error = false;
+    this.servicio.buscarPais(this.busqueda)
+      .subscribe({
+        next: (v) => console.log(v),
+        error : ({error}) => {
+          console.log(this._Error)
+          this._Error = true
+          console.log(this._Error)
+        },
+      })
+  }
+
+  ErrorBusqueda() : boolean{
+    return this._Error
   }
 }
