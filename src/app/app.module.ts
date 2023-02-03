bdc53d5953f233b8c5c2ef8f6d874909e99ce417
Pedro Moyano <pedro.moyano.garcia@gmail.com>
2023-02-03 16:11:46 +0100

    WIP view more about the country

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     PaisModule,
     SharedModule,
-    HttpClientModule
+    HttpClientModule,
     
   ],
   providers: [],
--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
   styleUrls: ['./ver-pais.component.sass']
 })
 export class VerPaisComponent {
+
+  constructor(private route : ActivatedRoute){}
 
 }
